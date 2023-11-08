c968a76ce17afb2613efb952d3d4bf66c379140b
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-08 22:41:34 +0900

    롬복, assertThat, TestUtil 도입

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tbTDD20231108
+
 명언 앱 TDD ( stream과 Optional을 최대한 활용, 영속성까지
---
+++ src/test/java/com/ll/standard/util/TestUtil.java
@@ -0,0 +1,31 @@
+package com.ll.standard.util;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class TestUtil {
+    // gen == generate 생성하다.
+    // 가짜 스캐너 만들기
+    public static Scanner genScanner(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        return new Scanner(in);
+    }
+
+    // 출력문 가져와서 하는 거
+    public static ByteArrayOutputStream setOutToByteArray() {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
