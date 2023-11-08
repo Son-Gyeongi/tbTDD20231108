6dad71b5075262dcf8eb37fbd0b8e2a92a9ae0bc
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-08 22:54:38 +0900

    t1

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # tbTDD20231108
 
 명언 앱 TDD ( stream과 Optional을 최대한 활용, 영속성까지
+
+### 프로젝트 구성
+- com.ll.global : 전체 모듈에서 공유하는
+- com.ll.domain : 모듈들
+- com.ll.standard : 다른 프로젝트에서도 쓸법한 공용 Utility
---
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -0,0 +1,27 @@
+package com.ll.global.app;
+
+import com.ll.standard.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    @DisplayName("프로그램 시작시에 \"== 명언 앱 ==\" 출력")
+    void t1() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        new App();
+
+        // new App()에서 실행되는 모든 출력문이 out으로 모인다.
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        assertThat(out)
+                .contains("== 명언 앱 ==");
+    }
+}
