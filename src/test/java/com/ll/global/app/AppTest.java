baf0aca8ee28779f658b4a57d03990e8ba282e7d
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-08 23:17:11 +0900

    t3, pass

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -65,7 +65,7 @@ public class AppTest {
         TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
 
         assertThat(out)
-                .contains("명언 : ")
-                .contains("작가 : ");
+                .contains("명언 :")
+                .contains("작가 :");
     }
 }
