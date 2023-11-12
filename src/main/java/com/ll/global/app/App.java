c838327cd8e78889ccdc42c6a3a0f9fd059c80dd
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-13 02:12:38 +0900

    mvc, w3

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -44,28 +44,7 @@ public class App {
                             );
                 }
                 case "수정" -> {
-                    long id = rq.getParameterAsLong("id", 0);
-
-                    quotations
-                            .stream()
-                            .filter(_quotation -> _quotation.getId() == id)
-                            .findFirst() // 하나라도 있으면 줘라. Optional로 준다.
-                            .ifPresentOrElse(
-                                    quotation -> {
-                                        System.out.println("명언(기존) : %s".formatted(quotation.getContent()));
-                                        System.out.println("명언 : ");
-                                        final String content = scanner.nextLine().trim();
-                                        System.out.println("작가(기존) : %s".formatted(quotation.getAuthorName()));
-                                        System.out.println("작가 : ");
-                                        final String authorName = scanner.nextLine().trim();
-
-                                        quotation.setContent(content);
-                                        quotation.setAuthorName(authorName);
-
-                                        System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
-                                    },
-                                    () -> System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id))
-                            );
+                    actionModify(rq);
                 }
                 case "등록" -> {
                     actionWrite(); // Show 라고 안 붙인 거는 CUD가 일어난다. Create, Update, Delete
@@ -78,6 +57,31 @@ public class App {
                 }
             }
         }
+    }
+
+    private void actionModify(final Rq rq) {
+        long id = rq.getParameterAsLong("id", 0);
+
+        quotations
+                .stream()
+                .filter(_quotation -> _quotation.getId() == id)
+                .findFirst() // 하나라도 있으면 줘라. Optional로 준다.
+                .ifPresentOrElse(
+                        quotation -> {
+                            System.out.println("명언(기존) : %s".formatted(quotation.getContent()));
+                            System.out.println("명언 : ");
+                            final String content = scanner.nextLine().trim();
+                            System.out.println("작가(기존) : %s".formatted(quotation.getAuthorName()));
+                            System.out.println("작가 : ");
+                            final String authorName = scanner.nextLine().trim();
+
+                            quotation.setContent(content);
+                            quotation.setAuthorName(authorName);
+
+                            System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
+                        },
+                        () -> System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id))
+                );
     }
 
     private void actionShowList() {
