68e8d0874e953fcd7ad405b812fcc3f6cd508c85
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-12 17:59:54 +0900

    RqTest, t1

---
+++ src/test/java/com/ll/global/rq/RqTest.java
@@ -0,0 +1,17 @@
+package com.ll.global.rq;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class RqTest {
+
+    @Test
+    @DisplayName("t1")
+    void t1() {
+        final Rq rq = new Rq("삭제?id=1");
+
+        assertThat(rq.getAction()).isEqualTo("삭제");
+    }
+}
