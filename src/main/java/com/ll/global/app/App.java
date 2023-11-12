a760978979d4ab13d66e70326f34d0e252bb9fdf
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-12 19:55:30 +0900

    t10, pass

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -33,7 +33,28 @@ public class App {
                     quotations
                             .removeIf(quotation -> quotation.getId() == id); // removeIf() 함수형
 
-                    System.out.printf("%d번 명언이 삭제되었습니다.", id);
+                    System.out.println("%d번 명언이 삭제되었습니다.".formatted(id));
+                }
+                case "수정" -> {
+                    long id = rq.getParameterAsLong("id", 0);
+
+                    final Quotation quotation = quotations
+                            .stream()
+                            .filter(_quotation -> _quotation.getId() == id)
+                            .findFirst() // 하나라도 있으면 줘라. Optional로 준다.
+                            .get();
+
+                    System.out.println("명언(기존) : %s".formatted(quotation.getContent()));
+                    System.out.println("명언 : ");
+                    final String content = scanner.nextLine().trim();
+                    System.out.println("작가(기존) : %s".formatted(quotation.getAuthorName()));
+                    System.out.println("작가 : ");
+                    final String authorName = scanner.nextLine().trim();
+
+                    quotation.setContent(content);
+                    quotation.setAuthorName(authorName);
+
+                    System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
                 }
                 case "등록" -> {
                     System.out.println("명언 : ");
