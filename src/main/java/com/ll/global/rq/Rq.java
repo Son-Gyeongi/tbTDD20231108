68352381ae622c4d04f30ad397942541dd13518d
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-12 18:02:50 +0900

    RqTest, t1, pass

--- src/main/java/com/ll/global/rq/Rq.java
+++ src/main/java/com/ll/global/rq/Rq.java
@@ -1,11 +1,13 @@
 package com.ll.global.rq;
 
 public class Rq {
+    private final String action;
     public Rq(String cmd) {
-
+        final String[] cmdBits = cmd.split("\\?", 2);
+        action = cmdBits[0].trim();
     }
 
     public String getAction() {
-        return null;
+        return action;
     }
 }
