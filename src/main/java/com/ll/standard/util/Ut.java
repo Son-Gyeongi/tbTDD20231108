3d229fecb632fbd53616aad81187c0ebd467bce9
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-13 11:40:48 +0900

    file, t1, pass

--- src/main/java/com/ll/standard/util/Ut.java
+++ src/main/java/com/ll/standard/util/Ut.java
@@ -2,23 +2,45 @@ package com.ll.standard.util;
 
 import lombok.SneakyThrows;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class Ut {
     // 내부 클래스
     public static class file {
 
         @SneakyThrows // try-catch를 자동으로 해준다.
         public static void save(String filePath, String content) {
+            Path path = Paths.get(filePath);
 
+            try {
+                // TRUNCATE_EXISTING : 해당 경로에 파일이 있으면 삭제하고 덮어쓰기
+                Files.writeString(Paths.get(filePath), content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            } catch (IOException e) {
+                // 부모 디렉토리가 없어서 발생한 예외인지 확인합니다.
+                Path parentDir = path.getParent();
+                if (parentDir != null && Files.notExists(parentDir)) {
+                    Files.createDirectories(parentDir);
+                    // 디렉토리를 생성한 후 다시 시도합니다.
+                    Files.writeString(Paths.get(filePath), content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+                } else {
+                    // 다른 종류의 IOException이면 그대로 예외를 던집니다.
+                    throw e;
+                }
+            }
         }
 
         // 파일이 있는지 체크
         public static boolean exists(String filePath) {
-            return false;
+            return Files.exists(Paths.get(filePath));
         }
 
         @SneakyThrows // try-catch를 자동으로 해준다.
         public static void delete(String filePath) {
-
+            Files.delete(Paths.get(filePath));
         }
     }
 }
