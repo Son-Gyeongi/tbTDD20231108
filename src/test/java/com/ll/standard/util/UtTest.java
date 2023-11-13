630dc2ef173bc7d7f4d2c8d0be4be3b97ec4ed25
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-13 11:54:28 +0900

    file, t2, refactoring

--- src/test/java/com/ll/standard/util/UtTest.java
+++ src/test/java/com/ll/standard/util/UtTest.java
@@ -1,39 +1,44 @@
 package com.ll.standard.util;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtTest {
-
     // 파일 테스트
-    @Test
-    @DisplayName("파일 생성, 삭제")
-    void t1() {
-        final String filePath = "temp/test.txt";
+
+    private final String filePath = "temp/test.txt";
+
+    // 테스트 메서드 전에 실행
+    @BeforeEach
+    void beforeEach() {
         // Utility 모아두는 곳 Ut
         Ut.file.save(filePath, "내용");
+    }
 
-        // 파일이 있는지 체크
-        assertThat(Ut.file.exists(filePath)).isTrue();
-
+    // 테스트 메서드 후에 실행
+    @AfterEach
+    void afterEach() {
         // 실행 후 삭제하기
         Ut.file.delete(filePath);
     }
 
     @Test
+    @DisplayName("파일 생성, 삭제")
+    void t1() {
+        // 파일이 있는지 체크
+        assertThat(Ut.file.exists(filePath)).isTrue();
+    }
+
+    @Test
     @DisplayName("파일내용 읽기")
     void t2() {
-        final String filePath = "temp/test.txt";
-        Ut.file.save(filePath, "내용");
-
         final String content = Ut.file.getContent(filePath);
 
         // 파일이 있는지 체크
         assertThat(content).isEqualTo("내용");
-
-        // 실행 후 삭제하기
-        Ut.file.delete(filePath);
     }
 }
