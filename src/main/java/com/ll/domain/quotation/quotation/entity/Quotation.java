e8eb330cab8d69a763b9d6bc7e3fb20dc6383aa7
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-14 01:31:32 +0900

    QuotationtionFileRepository, t3 refactoring

--- src/main/java/com/ll/domain/quotation/quotation/entity/Quotation.java
+++ src/main/java/com/ll/domain/quotation/quotation/entity/Quotation.java
@@ -2,16 +2,17 @@ package com.ll.domain.quotation.quotation.entity;
 
 import lombok.*;
 
-import java.util.Objects;
-
 @Getter
 //@AllArgsConstructor // 생성자에 모든 필드가 만들어진다.
 @RequiredArgsConstructor // final, @NonNull이 들어간 필드만 생성자로 만든다.
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 // jackson라이브러리 쓸 때 인자 없는 생성자가 있어야 한다. 굳이 공개할 필요없다.
 // jackson을 통해서 객체를 복원하겠다면 인자 없는 생성자가 있어야 한다.
+@ToString // 객체의 값이 문자열로 잘 나온다.
+@EqualsAndHashCode // 객체의 주소가 아닌 "id" 값으로 비교한다.
 public class Quotation {
     @Setter
+    @EqualsAndHashCode.Include // @EqualsAndHashCode(of = "id") 같다.
     private Long id;
     @Setter
     @NonNull
@@ -28,6 +29,7 @@ public class Quotation {
     아래와 같이 하면 Quotation 객체 2개를 같은지 다른지 오직 id가 같은 걸로 비교한다.
     id가 서로 같으면 같은 객체이다.
      */
+    /*
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -42,4 +44,5 @@ public class Quotation {
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
     }
+     */
 }
