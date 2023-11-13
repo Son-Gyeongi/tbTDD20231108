8518f936dd72849afc71d4925995cea32bfaad00
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-13 15:46:13 +0900

    QuotationRepository 4가지 기능

--- src/main/java/com/ll/domain/quotation/quotation/repository/QuotationRepository.java
+++ src/main/java/com/ll/domain/quotation/quotation/repository/QuotationRepository.java
@@ -9,11 +9,15 @@ import java.util.Optional;
 public interface QuotationRepository {
     // 인터페이스의 경우 추상 메서드만 넣을 수 있다. 메서드에 public 생략가능
 
+    // 명언 다 찾기
     List<Quotation> findAll();
 
+    // 명언 하나 삭제
     void delete(Quotation quotation);
 
+    // 명언 하나 찾기
     Optional<Quotation> findById(long id);
 
+    // 명언 저장(수정, 추가)
     void save(Quotation quotation);
 }
