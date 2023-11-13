e1ed43b76ec335a0e53279eb9a972bb9c6cc635e
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-13 11:47:17 +0900

    file, t2

--- src/main/java/com/ll/standard/util/Ut.java
+++ src/main/java/com/ll/standard/util/Ut.java
@@ -14,14 +14,14 @@ public class Ut {
 
         @SneakyThrows // try-catch를 자동으로 해준다.
         public static void save(String filePath, String content) {
-            Path path = Paths.get(filePath);
+            final Path path = Paths.get(filePath);
 
             try {
                 // TRUNCATE_EXISTING : 해당 경로에 파일이 있으면 삭제하고 덮어쓰기
                 Files.writeString(Paths.get(filePath), content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
             } catch (IOException e) {
                 // 부모 디렉토리가 없어서 발생한 예외인지 확인합니다.
-                Path parentDir = path.getParent();
+                final Path parentDir = path.getParent();
                 if (parentDir != null && Files.notExists(parentDir)) {
                     Files.createDirectories(parentDir);
                     // 디렉토리를 생성한 후 다시 시도합니다.
@@ -41,6 +41,10 @@ public class Ut {
         @SneakyThrows // try-catch를 자동으로 해준다.
         public static void delete(String filePath) {
             Files.delete(Paths.get(filePath));
+        }
+
+        public static String getContent(String filePath) {
+            return null;
         }
     }
 }
--- src/test/java/com/ll/standard/util/UtTest.java
+++ src/test/java/com/ll/standard/util/UtTest.java
@@ -9,14 +9,29 @@ public class UtTest {
 
     // 파일 테스트
     @Test
-    @DisplayName("파일 생성")
+    @DisplayName("파일 생성, 삭제")
     void t1() {
-        String filePath = "temp/test.txt";
+        final String filePath = "temp/test.txt";
         // Utility 모아두는 곳 Ut
         Ut.file.save(filePath, "내용");
 
         // 파일이 있는지 체크
         assertThat(Ut.file.exists(filePath)).isTrue();
+
+        // 실행 후 삭제하기
+        Ut.file.delete(filePath);
+    }
+
+    @Test
+    @DisplayName("파일내용 읽기")
+    void t2() {
+        final String filePath = "temp/test.txt";
+        Ut.file.save(filePath, "내용");
+
+        final String content = Ut.file.getContent(filePath);
+
+        // 파일이 있는지 체크
+        assertThat(content).isEqualTo("내용");
 
         // 실행 후 삭제하기
         Ut.file.delete(filePath);
